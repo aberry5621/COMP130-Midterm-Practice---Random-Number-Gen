8c168894b9509df9f273d10077b3b848f88fc134
alexberry721 <alexberry721@gmail.com>
2016-10-18 15:07:11 -0700

    Random nums 10-100

--- COMP130 Midterm Practice - Random Number Gen/main.cpp
+++ COMP130 Midterm Practice - Random Number Gen/main.cpp
@@ -6,10 +6,111 @@
 //  Copyright © 2016 COMP130. All rights reserved.
 //
 
-#include <iostream>
+#define prog 2
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+#if (prog==2)
+
+#include<iostream>
+#include<ctime>
+using namespace std;
+
+int main() {
+    
+    cout << "Less Random but Still So. \n";
+    
+    // generate random numbers between 10 and 100
+    
+    // random seed
+    srand(time(0));
+    
+    // boundaries
+    int l_bound = 10;
+    int u_bound = 100;
+    
+    cout << "Loop attempt at 10 random numbers between 10 and 100: " << endl;
+    
+    for (int i = 0; i < 100; i++) {
+        
+        // lb + r % (ub-lb+1)
+        int rand_num_btwn_10_100 = l_bound + rand() % (u_bound - l_bound + 1);
+        
+        cout << "time: " << time(0) << " rand: " << rand_num_btwn_10_100 << endl;
+    }
+    
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#elif (prog==1)
+
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+    
+    cout << "So Random!\n";
+    
+    // seed
+    srand(time(0));
+    
+    int rand_1 = rand();
+    
+    cout << rand_1 << endl;
+    
+    cout << rand_1 << endl;
+    
+    cout << rand_1 << endl;
+    
+    cout << rand_1 << endl;
+    
+    
+    
+    
+    
+    
+    
+    return 0;
+}
+
+
+#endif
+
