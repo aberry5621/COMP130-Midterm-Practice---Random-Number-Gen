9bc75dd2e061a7651ff4eeb111954109393fb986
alexberry721 <alexberry721@gmail.com>
2016-10-17 20:50:54 -0700

    Initial Commit

---
+++ COMP130 Midterm Practice - Random Number Gen/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  COMP130 Midterm Practice - Random Number Gen
+//
+//  Created by ax on 10/17/16.
+//  Copyright © 2016 COMP130. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
